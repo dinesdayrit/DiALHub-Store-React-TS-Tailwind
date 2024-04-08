341084d116b6c6640ef190c4dea1d16095f228b9
dinesdayrit <dayritdines22@gmail.com>
2024-04-08 21:59:00 +0800

    add navbar activechecker

--- src/components/common/MainNavigation.tsx
+++ src/components/common/MainNavigation.tsx
@@ -9,6 +9,12 @@ export default function MainNavigation() {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  function isActiveChecker(isActive:boolean) {
+    return isActive ? "mr-8 text-white hover:text-orange-300 font-semibold text-xl border-b border-yellow-300 p-2" : "mr-8 text-white hover:text-orange-300 font-semibold text-xl";
+}
+
+
+
   return (
     <div className="py-6 px-3 lg:px-24 bg-gray-700 flex justify-between">
       <NavLink to="" className="text-white font-serif font-bold lg:text-3xl">DiAL</NavLink>
@@ -18,10 +24,10 @@ export default function MainNavigation() {
         
         {/* Navigation links */}
         <div className={`flex flex-col lg:flex-row lg:block ${isMenuOpen ? '' : 'hidden'}`}>
-          <NavLink to="" className="mr-8 text-white hover:text-orange-300 font-semibold text-xl">Home</NavLink>
-          <NavLink to="about" className="mr-8 text-white hover:text-orange-300 font-semibold text-xl">About</NavLink>
-          <NavLink to="products" className="mr-8 text-white hover:text-orange-300 font-semibold text-xl">Products</NavLink>
-          <NavLink to="login" className="text-white hover:text-orange-300 font-semibold text-xl">Login</NavLink>
+          <NavLink to="" className={({isActive}) => isActiveChecker(isActive)}>Home</NavLink>
+          <NavLink to="about" className={({isActive}) => isActiveChecker(isActive)}>About</NavLink>
+          <NavLink to="products" className={({isActive}) => isActiveChecker(isActive)}>Products</NavLink>
+          <NavLink to="login" className={({isActive}) => isActiveChecker(isActive)}>Login</NavLink>
         </div>
       </div>
     </div>
