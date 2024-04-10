f0f049e7e22d411f9fa4f7e8da3b2299fc1e631c
dinesdayrit <dayritdines22@gmail.com>
2024-04-10 15:14:25 +0800

    adjust styling for featured products

--- src/components/homepage/FeaturedProducts.tsx
+++ src/components/homepage/FeaturedProducts.tsx
@@ -9,16 +9,18 @@ const FeaturedProducts: FC = () => {
         <div className="flex flex-col items-center mt-16 mb-16 justify-center">
             <h1 className="mb-4 text-5xl">Featured Products...</h1>
 
-            <div className="flex flex-col justify-center items-center">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+            <div className="flex flex-col justify-center items-center mt-16">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-6">
                     {featuredProducts.map((item) => (
-                        <div key={item.id} className="flex flex-col items-center "> 
-                            <img src={item.image} alt={item.name} width={300} className='h-1/2 w-10/12 rounded-lg' /> 
-                            <h2>{item.name}</h2>
-                            <p>{item.description}</p>
-                            <p>Price: ${item.price.toFixed(2)}</p>
-                            <button className='bg-green-500 p-3 rounded-lg text-white'>Add to cart</button>
-                            
+                        <div key={item.id} className="flex flex-col items-center"> 
+                            <img src={item.image} alt={item.name} width={300} className='h-1/2 w-10/12 rounded-lg mb-4' /> 
+
+                            <div className='flex flex-col items-center justify-center w-10/12 h-40'>
+                                <h2 className="text-center font-bold">{item.name}</h2>
+                                <p className="text-center">{item.description}</p>
+                                <p className="text-center">Price: ${item.price.toFixed(2)}</p>
+                                <button className='bg-green-500 p-3 rounded-lg text-white'>Add to cart</button>
+                            </div>
                         </div>
                     ))}
                 </div>
