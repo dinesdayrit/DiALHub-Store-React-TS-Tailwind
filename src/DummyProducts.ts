8f316132561cdb2ba7d9367f1be4674db7f48870
dinesdayrit <dayritdines22@gmail.com>
2024-04-10 14:53:10 +0800

    add featrured products

---
+++ src/DummyProducts.ts
@@ -0,0 +1,52 @@
+const DUMMY_PRODUCTS = [
+    {   
+        id: '1',
+        image: './sambaShoes.png',
+        name: 'Adidas Samba',
+        price: 3000,
+        description: 'Size 7',
+        
+    },
+    {
+        id: '2',
+        image: './nikeBball.png',
+        name: 'Nike Impact 4',
+        price: 3000,
+        description: 'Colour Shown: Dusty Cactus/Thunder Blue/Sea Glass/University Gold',
+        
+    },
+    {
+        id: '3',
+        image: './nikeBball2.png',
+        name: 'Nike Impact 4',
+        price: 3000,
+        description: 'Colour Shown: Black/Armoury Navy/Pure Platinum/Volt',
+     
+    },
+    {
+        id: '4',
+        image: './nikeBball2.png',
+        name: 'Matte Black',
+        price: 3000,
+        description: 'Standard Black Card',
+     
+    },
+    {
+        id: '5',
+        image: './shopping-card-yellow.svg',
+        name: 'Matte Black',
+        price: 3000,
+        description: 'Standard Black Card',
+     
+    },
+    {
+        id: '6',
+        image: './shopping-card-blue.svg',
+        name: 'Matte Black',
+        price: 3000,
+        description: 'Standard Black Card',
+      
+    },
+];
+
+export default DUMMY_PRODUCTS;
--- src/components/homepage/FeaturedProducts.tsx
+++ src/components/homepage/FeaturedProducts.tsx
@@ -1,13 +1,31 @@
-export default function FeaturedProducts() {
+import { FC } from 'react';
+import DUMMY_PRODUCTS from "@/DummyProducts";
+
+const FeaturedProducts: FC = () => {
+    // Get the first three products
+    const featuredProducts = DUMMY_PRODUCTS.slice(0, 3);
+
     return (
-        <div className="flex flex-col items-center mt-16 mb-16">
-            <h1 className="mb-4 text-5xl">Featured Products</h1>
-            <ul className="flex flex-row gap-10">
-                <li>P1</li>
-                <li>P2</li>
-                <li>P3</li>
-                <li>See All...</li>
-            </ul>
+        <div className="flex flex-col items-center mt-16 mb-16 justify-center">
+            <h1 className="mb-4 text-5xl">Featured Products...</h1>
+
+            <div className="flex flex-col justify-center items-center">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                    {featuredProducts.map((item) => (
+                        <div key={item.id} className="flex flex-col items-center "> 
+                            <img src={item.image} alt={item.name} width={300} className='h-1/2 w-10/12 rounded-lg' /> 
+                            <h2>{item.name}</h2>
+                            <p>{item.description}</p>
+                            <p>Price: ${item.price.toFixed(2)}</p>
+                            <button className='bg-green-500 p-3 rounded-lg text-white'>Add to cart</button>
+                            
+                        </div>
+                    ))}
+                </div>
+                <button className="hover:text-green-500">More Products &gt;&gt;</button>   
+            </div>
         </div>
     );
-}
+};
+
+export default FeaturedProducts;
