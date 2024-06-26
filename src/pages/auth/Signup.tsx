4ad31d745f37d661cb9109b1ea775a0df2f75808
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-06-26 17:20:09 +0800

    feat: registration form pic on large screen

--- src/pages/auth/Signup.tsx
+++ src/pages/auth/Signup.tsx
@@ -3,6 +3,7 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { formSchema, formAttributes } from './Schema/Formschema';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
   const { register, handleSubmit, formState: { errors }, reset } = useForm<z.infer<typeof formSchema>>({
@@ -21,8 +22,9 @@ const Signup = () => {
   };
 
   return (
-    <div className="flex justify-center md:mt-16 lg:18">
-        <div className="flex flex-col lg:justify-center p-12 rounded-md lg:w-[30rem] lg:h-auto h-screen w-screen border shadow-xl">
+    <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center h-screen">
+
+        <div className="flex flex-col lg:justify-center p-12 h-full items-center">
         <div className="flex flex-col items-center">
         <h1 className="font-bold text-3xl text-green-500">Register</h1>
         </div>
@@ -39,17 +41,21 @@ const Signup = () => {
             {errors[attribute.name]?.message && <p className="text-red-500 mt-1">{errors[attribute.name]?.message}</p>}
           </div>
         ))}
+        <p className="text-end mt-4">Already have an account? <Link to="/login" className="text-green-500 underline">login here </Link> </p>
         <button type="submit" className="bg-green-400 mt-4 rounded-lg text-white py-2">Sign Up</button>
         <div>
-        
-          <label>
+          <label className="flex gap-1">
             <input type="checkbox" {...register('privacyPolicy')} />
-            I acknowledge the privacy policy
+             I acknowledge the privacy policy
           </label>
           {errors.privacyPolicy && <p className="text-red-500 mt-1">{errors.privacyPolicy.message}</p>}
         </div>
         
       </form>
+      </div>
+
+      <div className="hidden md:block w-full h-full 2">
+        <img src='/loginpic.jpg' alt="/" className="h-full w-full bg-cover"/>
       </div>
     </div>
   );
