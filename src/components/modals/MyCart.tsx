e7a8ed47dcf73427de24d570cd0169c9dbdefed0
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-06-29 11:38:10 +0800

    feat: add MyCart component

---
+++ src/components/modals/MyCart.tsx
@@ -0,0 +1,13 @@
+export default function MyCart() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center backdrop-blur-sm">
+    <div className="relative flex items-center justify-center">
+        <h1>My Cart</h1>
+        <p>Item 1</p>
+        <p>Item 2</p>
+        <button>Checkout</button>
+        <button>Close</button>
+    </div>
+    </div>
+  )
+}
