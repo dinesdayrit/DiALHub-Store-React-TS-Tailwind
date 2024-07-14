103e19860939ca0d088cd4ec1ceb07e1e1d4e46c
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-07-14 13:06:08 +0800

    style: adjust hight and max-w in about section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,19 +1,21 @@
+export default function About() {
+  return (
+    <div className="flex flex-col items-center h-[40rem] ">
+      <h1 className="mt-16 font-bold text-3xl">About us.</h1>
 
-export default function About () {
-    return (
-        <div className="flex flex-col items-center h-screen">
-            <h1 className="mt-16 font-bold text-3xl">About us.</h1>
-
-            <div className="flex flex-col lg:flex-row items-center w-1/2"> 
-            <img src="/fam-pic.svg" width={200}/>
-            <p className="text-mono">
-            <span className="text-2xl text-amber-700">DiAL </span> 
-            stands for <span className="text-xl text-amber-600">Di</span>nes, 
-            <span className="text-xl text-amber-600">A</span>lyssa,
-             and <span className="text-xl text-amber-600">L</span>ucas...
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
-            </div>
-        </div>
-    )
+      <div className="flex flex-col lg:flex-row items-center justify-center w-1/2 max-w-6xl">
+        <img src="/fam-pic.svg" width={200} />
+        <p className="text-mono">
+          <span className="text-2xl text-amber-700">DiAL </span>
+          stands for <span className="text-xl text-amber-600">Di</span>nes,
+          <span className="text-xl text-amber-600">A</span>lyssa, and{" "}
+          <span className="text-xl text-amber-600">L</span>ucas... Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat.
+        </p>
+      </div>
+    </div>
+  );
 }
