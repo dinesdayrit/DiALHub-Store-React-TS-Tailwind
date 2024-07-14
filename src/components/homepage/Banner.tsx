4d9ea4dfd73547733656b1c85c397ee78dec7645
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-07-14 12:59:31 +0800

    style: adjust banner max hieght

--- src/components/homepage/Banner.tsx
+++ src/components/homepage/Banner.tsx
@@ -1,11 +1,13 @@
 export default function Banner() {
-    return (
-        <div>
-            <div className="relative" >
-            <img src='/banner-bg.png' className="max-h-[40rem] w-full" />
-            <img src='/dial-logo.png' className=" w-[5rem] lg:w-[20rem] z-1 absolute lg:top-10 top-5" />
-            </div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <div className="relative">
+        <img src="/banner-bg.png" className="max-h-[43rem] w-full" />
+        <img
+          src="/dial-logo.png"
+          className=" w-[5rem] lg:w-[20rem] z-1 absolute lg:top-10 top-5"
+        />
+      </div>
+    </div>
+  );
 }
