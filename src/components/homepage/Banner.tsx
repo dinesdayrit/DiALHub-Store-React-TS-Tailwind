a18b4a32ec5d4d08c3751a7ace546c25cfdd5c4c
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-07-18 11:33:42 +0800

    style: add slide-up animation on hero sections logo

--- src/components/homepage/Banner.tsx
+++ src/components/homepage/Banner.tsx
@@ -5,7 +5,7 @@ export default function Banner() {
         <img src="/banner-bg.png" className="max-h-[43rem] w-full" />
         <img
           src="/dial-logo.png"
-          className=" w-[5rem] lg:w-[20rem] z-1 absolute lg:top-10 top-5"
+          className=" w-[5rem] lg:w-[20rem] z-1 absolute lg:top-10 top-5 animate-slide-up-fade-in"
         />
       </div>
     </div>
