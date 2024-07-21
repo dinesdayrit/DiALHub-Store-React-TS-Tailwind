5fbc990d7c5d8106419552cd53e0b2bfcf91d829
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-07-21 12:10:08 +0800

    style: fixing hero section

--- src/components/homepage/Banner.tsx
+++ src/components/homepage/Banner.tsx
@@ -1,11 +1,16 @@
 export default function Banner() {
   return (
     <div>
-      <img src="/banner-bg.png" className="max-h-[43rem]  w-full" />
-      {/* <img
+      <div className="relative">
+        <img
+          src="/banner-bg.png"
+          className="max-h-[43rem]  w-full animate-slide-up-fade-in"
+        />
+        <img
           src="/dial-logo.png"
-          className=" w-[5rem] lg:w-[20rem] z-1  lg:top-10 top-5 animate-slide-up-fade-in"
-        /> */}
+          className=" w-[5rem] lg:w-[20rem] z-1 absolute  lg:top-10 top-5 animate-slide-up-fade-in"
+        />
+      </div>
     </div>
   );
 }
