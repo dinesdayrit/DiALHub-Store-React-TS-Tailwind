99115bbcdf7a77667af10a38028cfd75e19d6c70
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-07-26 11:19:31 +0800

    style: update products page

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -3,16 +3,19 @@ import AddToCartButton from "../shared/AddtoCartButton";
 
 const Products = () => {
   return (
-    <div className="flex flex-col items-center p-16 gap-8">
+    <div className="flex flex-col items-center p-8 gap-4">
       <h1 className="font-bold text-4xl">PRODUCTS</h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-6 px-4 border rounded-md shadow-md">
         {DUMMY_PRODUCTS.map((product) => (
-          <div key={product.id} className="border rounded-lg">
+          <div
+            key={product.id}
+            className="border rounded-lg flex flex-col items-center shadow-lg"
+          >
             <img
               src={product.image}
               alt={product.name}
-              className="w-[25rem] h-[25rem] object-cover rounded-t-lg"
+              className="w-full h-[25rem] object-cover rounded-t-lg flex-shrink-0"
             />
             <div className="flex flex-col items-center justify-center w-10/12 h-40">
               <h2 className="text-xl font-semibold">{product.name}</h2>
