28d33151dd57da1b6bcfc965d52ef09573792de1
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-07-27 14:54:54 +0800

    style: animate when featured product is on screen

--- src/components/homepage/FeaturedProducts.tsx
+++ src/components/homepage/FeaturedProducts.tsx
@@ -2,20 +2,45 @@ import { FC } from "react";
 import { Link } from "react-router-dom";
 import DUMMY_PRODUCTS from "@/DummyProducts";
 import AddToCartButton from "../shared/AddtoCartButton";
+import { motion, Variants } from "framer-motion";
 
 const FeaturedProducts: FC = () => {
   // Get the first three products
   const featuredProducts = DUMMY_PRODUCTS.slice(0, 3);
+
+  const cardVariants: Variants = {
+    offscreen: {
+      y: 100,
+      opacity: 0,
+    },
+    onscreen: {
+      y: 0,
+      opacity: 1,
+      rotateY: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.8,
+      },
+    },
+  };
 
   return (
     <div className="flex flex-col items-center mt-16 mb-16 justify-center">
       <h1 className="mb-4 text-3xl md:text-5xl">Featured Products...</h1>
 
       <div className="flex flex-col justify-center items-center mt-16">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-6">
+        <motion.div
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: false, amount: 0.1 }}
+          variants={cardVariants}
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-6"
+        >
           {featuredProducts.map((product) => (
             <div key={product.id} className="flex flex-col items-center">
-              <img
+              <motion.img
+                whileHover={{ scale: 1.1 }}
                 src={product.image}
                 alt={product.name}
                 width={300}
@@ -34,7 +59,7 @@ const FeaturedProducts: FC = () => {
               </div>
             </div>
           ))}
-        </div>
+        </motion.div>
         <Link className="hover:text-green-500 hover:font-medium" to="products">
           More Products &gt;&gt;
         </Link>
