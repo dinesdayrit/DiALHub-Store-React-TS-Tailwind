d429f94d5bbada78bc662d7194bc0036593ab936
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-07-27 14:44:29 +0800

    style: rotate animation on hero logo

--- src/components/homepage/Banner.tsx
+++ src/components/homepage/Banner.tsx
@@ -1,4 +1,15 @@
+import { motion, Variants } from "framer-motion";
+
 export default function Banner() {
+  const imageVariants: Variants = {
+    hidden: { rotateY: 120 },
+    visible: {
+      rotateY: 0,
+      transition: {
+        duration: 1,
+      },
+    },
+  };
   return (
     <div>
       <div className="relative">
@@ -6,9 +17,14 @@ export default function Banner() {
           src="/banner-bg.png"
           className="max-h-[43rem]  w-full animate-slide-up-fade-in"
         />
-        <img
+        <div></div>
+        <motion.img
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: false, amount: 0.5 }}
+          variants={imageVariants}
           src="/dial-logo.png"
-          className=" w-[5rem] lg:w-[20rem] z-1 absolute  lg:top-10 top-5 animate-slide-up-fade-in"
+          className=" w-[5rem] lg:w-[20rem] z-1 absolute  lg:top-10 top-5"
         />
       </div>
     </div>
