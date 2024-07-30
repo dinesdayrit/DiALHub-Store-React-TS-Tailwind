45a5fb80b445126d1da1f61d1716f889422d2f67
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-07-30 12:51:19 +0800

    style: update animation on hero section

--- src/components/homepage/Hero.tsx
+++ src/components/homepage/Hero.tsx
@@ -1,8 +1,42 @@
 import { ArrowUpRight } from "lucide-react";
 import { Button } from "../ui/button";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 
 export default function Hero() {
+  const h1textVariants: Variants = {
+    offscreen: {
+      x: 50,
+      opacity: 0,
+    },
+    onscreen: {
+      x: 0,
+      opacity: 1,
+      rotateY: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 2,
+      },
+    },
+  };
+
+  const pVariants: Variants = {
+    offscreen: {
+      x: -50,
+      opacity: 0,
+    },
+    onscreen: {
+      x: 0,
+      opacity: 1,
+      rotateY: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.4,
+        duration: 3,
+      },
+    },
+  };
+
   return (
     <div className="relative h-[70vh]">
       <div
@@ -11,10 +45,24 @@ export default function Hero() {
       ></div>
       <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-60">
         <div className="flex flex-col items-center">
-          <h1 className="font-semibold font-serif text-slate-100 text-3xl md:text-7xl">
+          <motion.h1
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ once: false, amount: 0.1 }}
+            variants={h1textVariants}
+            className="font-semibold font-serif text-slate-100 text-3xl md:text-7xl"
+          >
             The DiAL Hub
-          </h1>
-          <p className="text-slate-200 text-xl">Smart Shop</p>
+          </motion.h1>
+          <motion.p
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ once: false, amount: 0.1 }}
+            variants={pVariants}
+            className="text-slate-200 text-xl"
+          >
+            Smart Shop
+          </motion.p>
           <p className="mt-4 text-slate-100 text-center px-8">
             Experience the Ultimate Shopping Destination, Where Innovation Meets
             Style for Unmatched Convenience and Quality! <br />
